a2bb9f54baa797d6679d31764b8d36ce29e13b04
Leranth <leranthhend@gmail.com>
2025-01-11 17:31:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     #print(len(file_contents.split()))
     lowered_string = file_contents.lower()
+    print("--- Begin report of books/frankenstein.txt ---")
+
     for char in lowered_string:
         if char in char_dict:
             char_dict[char] += 1
@@ -10,4 +12,5 @@ with open("books/frankenstein.txt") as f:
             char_dict[char] = 1
     for element in char_dict:
         print(f"The '{element}' character was found {char_dict[element]} times")
+    print("--- End report ---")
     # for frodo
