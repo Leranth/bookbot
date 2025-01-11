cd907cffbc7efa7beddad71a232e2ed9202d8595
Leranth <leranthhend@gmail.com>
2025-01-11 17:03:00 +0100

    main changed to count characters from a book

---
+++ main.py
@@ -0,0 +1,13 @@
+with open("books/frankenstein.txt") as f:
+    char_dict = {}
+    file_contents = f.read()
+    #print(len(file_contents.split()))
+    lowered_string = file_contents.lower()
+    for char in lowered_string:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    for element in char_dict:
+        print(f"The '{element}' character was found {char_dict[element]} times")
+    # for frodo
